9791d6fd5bf1a05fa902a3442335b2b0fb0114a4
DANTE <104479517+xflydan@users.noreply.github.com>
2023-12-06 06:27:42 +0700

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,15 +57,15 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYM")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("zxcpy")
+                await self.one.join_chat("DantePedia")
+                await self.one.join_chat("dansupport")
             except:
                 pass
             assistants.append(1)
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "BOT AKTIF KONTOL 🔥"
                 )
             except:
                 LOGGER(__name__).error(
@@ -88,15 +88,15 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYM")
-                await self.two.join_chat("TheYukki")
-                await self.two.join_chat("YukkiSupport")
+                await self.one.join_chat("zxcpy")
+                await self.one.join_chat("DantePedia")
+                await self.one.join_chat("dansupport")
             except:
                 pass
             assistants.append(2)
             try:
                 await self.two.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "BOT AKTIF KONTOL 🔥"
                 )
             except:
                 LOGGER(__name__).error(
@@ -119,15 +119,15 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYM")
-                await self.three.join_chat("TheYukki")
-                await self.three.join_chat("YukkiSupport")
+                await self.three.join_chat("zxcpy")
+                await self.three.join_chat("DantePedia")
+                await self.three.join_chat("dansupport")
             except:
                 pass
             assistants.append(3)
             try:
                 await self.three.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "BOT AKTIF KONTOL 🔥"
                 )
             except:
                 LOGGER(__name__).error(
@@ -150,15 +150,15 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TeamYM")
-                await self.four.join_chat("TheYukki")
-                await self.four.join_chat("YukkiSupport")
+                await self.three.join_chat("zxcpy")
+                await self.three.join_chat("DantePedia")
+                await self.three.join_chat("dansupport")
             except:
                 pass
             assistants.append(4)
             try:
                 await self.four.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "BOT AKTIF KONTOL 🔥"
                 )
             except:
                 LOGGER(__name__).error(
@@ -181,15 +181,15 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TeamYM")
-                await self.five.join_chat("TheYukki")
-                await self.five.join_chat("YukkiSupport")
+                await self.three.join_chat("zxcpy")
+                await self.three.join_chat("DantePedia")
+                await self.three.join_chat("dansupport")
             except:
                 pass
             assistants.append(5)
             try:
                 await self.five.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "BOT AKTIF KONTOL 🔥"
                 )
             except:
                 LOGGER(__name__).error(
